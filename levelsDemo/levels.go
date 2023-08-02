d4c5dc3a6e2ae08d83ce5fa95a790e651705791d
Laurent Demailly <ldemailly@gmail.com>
2023-08-02 16:39:23 -0700

    Changing color output again

--- levelsDemo/levels.go
+++ levelsDemo/levels.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main() {
+	log.SetLogLevelQuiet(log.Debug)
+	log.Config.LogFileAndLine = false
+	log.Config.LogPrefix = ""
+	log.Config.GoroutineID = false
+	log.Debugf("This is a debug message without goroutine id, file:line nor prefix (cli style)")
+	log.Config = log.DefaultConfig()
 	// So log fatal doesn't panic nor exit (so we can print the non json last line).
 	log.Config.FatalPanics = false
 	log.Config.FatalExit = func(int) {}
-	log.SetLogLevelQuiet(log.Debug)
 	// Meat of the example: (some of these are reproducing fixed issues in `logc` json->console attributes detection)
-	log.Debugf("This is a debug message ending with backslash \\")
+	log.Debugf("Back to default (server) logging style with a debug message ending with backslash \\")
 	log.LogVf("This is a verbose message")
 	log.Printf("This an always printed, file:line omitted message (and no level in console)")
 	log.Infof("This is an info message with no attributes but with \"quotes\"...")
